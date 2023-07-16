c2f5a7419cb93161aa53b50e56947c8f60cf007d
david.plant <48261546+david.plant@users.noreply.github.com>
2023-07-16 09:51:06 +0100

    remove langauge dropdown

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -145,13 +145,6 @@ export default {
         }
       }
     },
-    setLanguage(locale, country, flag) {
-      this.lan = locale;
-      this.text = country;
-      this.flag = flag;
-      document.getElementById("header-lang-img").setAttribute("src", flag);
-      i18n.global.locale = locale;
-    },
     toggleDarkMode() {
       if (document.documentElement.getAttribute("data-layout-mode") == "dark") {
         document.documentElement.setAttribute("data-layout-mode", "light");
@@ -160,46 +153,6 @@ export default {
       }
     },
     initTopbarComponents() {
-      function updateCartPrice() {
-        var currencySign = "$";
-        var subtotal = 0;
-        Array.from(document.getElementsByClassName("cart-item-price")).forEach(function (e) {
-          subtotal += parseFloat(e.innerHTML);
-        });
-        if (document.getElementById("cart-item-total")) {
-          document.getElementById("cart-item-total").innerHTML = currencySign + subtotal.toFixed(2);
-        }
-      }
-      if (document.getElementsByClassName("dropdown-item-cart")) {
-        var dropdownItemCart = document.querySelectorAll(".dropdown-item-cart").length;
-        Array.from(document.querySelectorAll("#page-topbar .dropdown-menu-cart .remove-item-btn")).forEach(function (item) {
-          item.addEventListener("click", function () {
-            dropdownItemCart--;
-            this.closest(".dropdown-item-cart").remove();
-            Array.from(document.getElementsByClassName("cartitem-badge")).forEach(function (e) {
-              e.innerHTML = dropdownItemCart;
-            });
-            updateCartPrice();
-            if (document.getElementById("empty-cart")) {
-              document.getElementById("empty-cart").style.display = dropdownItemCart == 0 ? "block" : "none";
-            }
-            if (document.getElementById("checkout-elem")) {
-              document.getElementById("checkout-elem").style.display = dropdownItemCart == 0 ? "none" : "block";
-            }
-          });
-        });
-        Array.from(document.getElementsByClassName("cartitem-badge")).forEach(function (e) {
-          e.innerHTML = dropdownItemCart;
-        });
-        if (document.getElementById("empty-cart")) {
-          document.getElementById("empty-cart").style.display = "none";
-        }
-        if (document.getElementById("checkout-elem")) {
-          document.getElementById("checkout-elem").style.display = "block";
-        }
-        updateCartPrice();
-      }
-
       // notification messages
       if (document.getElementsByClassName("notification-check")) {
         Array.from(document.querySelectorAll(".notification-check input")).forEach(function (element) {
@@ -212,15 +165,6 @@ export default {
   },
   computed: {},
   mounted() {
-    if (process.env.VUE_APP_I18N_LOCALE) {
-      this.flag = process.env.VUE_APP_I18N_LOCALE;
-      this.languages.forEach((item) => {
-        if (item.language == this.flag) {
-          document.getElementById("header-lang-img").setAttribute("src", item.flag);
-        }
-      });
-    }
-
     document.addEventListener("scroll", function () {
       var pageTopbar = document.getElementById("page-topbar");
       if (pageTopbar) {
@@ -271,44 +215,6 @@ export default {
         </div>
 
         <div class="d-flex align-items-center">
-          <div class="dropdown d-md-none topbar-head-dropdown header-item">
-            <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle"
-              id="page-header-search-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="bx bx-search fs-22"></i>
-            </button>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-              aria-labelledby="page-header-search-dropdown">
-              <form class="p-3">
-                <div class="form-group m-0">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search ..."
-                      aria-label="Recipient's username" />
-                    <b-button variant="primary" type="submit">
-                      <i class="mdi mdi-magnify"></i>
-                    </b-button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <div class="dropdown ms-1 topbar-head-dropdown header-item">
-            <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle"
-              data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <img id="header-lang-img" src="@/assets/images/flags/us.svg" alt="Header Language" height="20"
-                class="rounded" />
-            </button>
-            <div class="dropdown-menu dropdown-menu-end">
-              <b-link href="javascript:void(0);" v-for="(entry, i) in languages" :key="`Lang${i}`" :value="entry"
-                @click="setLanguage(entry.language, entry.title, entry.flag)"
-                :class="{ active: lan === entry.language }" class="dropdown-item notify-item language py-2"
-                data-lang="en" title="English">
-                <img :src="entry.flag" alt="user-image" class="me-2 rounded" height="18" />
-                <span class="align-middle">{{ entry.title }}</span>
-              </b-link>
-            </div>
-          </div>
-
           <div class="dropdown topbar-head-dropdown ms-1 header-item">
             <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle"
               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
