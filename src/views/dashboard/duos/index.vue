58203d9d8c6b84ca1e3362e878aadb4f55cf4e55
david.plant <48261546+david.plant@users.noreply.github.com>
2023-07-20 21:21:56 +0100

    duos working

--- src/views/dashboard/duos/index.vue
+++ src/views/dashboard/duos/index.vue
@@ -3,10 +3,12 @@
 import Layout from "../../../layouts/main.vue";
 import PageHeader from "@/components/page-header";
 import appConfig from "../../../../app.config";
+import { format, subYears, eachMonthOfInterval, endOfMonth, startOfMonth } from 'date-fns';
+import axios from 'axios';
 
 export default {
   page: {
-    title: "Distribution Use Of System (DUoS)",
+    title: "Distribution Use of System (DUoS)",
     meta: [{ name: "description", content: appConfig.description }],
   },
   data() {
@@ -14,7 +16,7 @@ export default {
       title: "Distribution Use of System (DUoS)",
       items: [
         {
-          text: "Industry Invoices",
+          text: "Industry Flows",
           href: "/",
         },
         {
@@ -23,35 +25,79 @@ export default {
         },
       ],
       deliveryMonths:[],
-      selectedMonth: ''
+      selectedMonthType: null,
+      selectedMonth: null,
+      selectedFromDate: null,
+      selectedToDate: null,
+      mpanList: null,
+      selectedMpan: null,
+      duosData: null,
     };
   },
   components: {
     Layout,
     PageHeader
   },
+  computed:{
+    isDataAvailable() {
+      return !!this.duosData && this.duosData.length > 0;
+    }, 
+  },  
   methods:{
+    async fetchDuosData() {
+      console.log('fetching data');
+      var url = ''
+      if(this.selectedMonthType == "1")
+      {
+        url = `http://gedv-rtpsfc.gazpromuk.intra:19081/DV_FlexPortalApi/flexportal_api/duos/ByDeliveryDate/${this.selectedMpan}/${this.selectedFromDate}/${this.selectedToDate}`;
+      }
+      else
+      {
+        url = `http://gedv-rtpsfc.gazpromuk.intra:19081/DV_FlexPortalApi/flexportal_api/duos/ByInvoiceDate/${this.selectedMpan}/${this.selectedFromDate}/${this.selectedToDate}`;
+      }
+      try {
+        const response = await axios.get(url);
+        this.duosData = response.data;
+        console.log(this.duosData);
+      } catch (error) {
+        console.error('Error fetching duos data:', error);
+      }
+    }, 
+    async fetchMpanData() {
+      const url = 'http://gedv-rtpsfc.gazpromuk.intra:19081/DV_FlexPortalApi/flexportal_api/mpan/';
+      try {
+        const response = await axios.get(url);
+        this.mpanList = response.data;
+      } catch (error) {
+        console.error('Error fetching Mpan data:', error);
+      }
+    },    
     generateDeliveryMonthArray() {
       const currentDate = new Date();
-      const currentYear = currentDate.getFullYear();
-      const currentMonth = currentDate.getMonth();
+      const sixYearsAgo = subYears(currentDate, 6);
+      const allMonths = eachMonthOfInterval({ start: sixYearsAgo, end: currentDate });
 
-      const monthYearArray = [];
-
-      for (let i = 0; i < 72; i++) {
-      const year = currentYear - Math.floor(i / 12);
-      const month = (currentMonth - i % 12 + 12) % 12;
-      const monthName = new Date(year, month).toLocaleString('default', { month: 'long' });
-
-      const monthYearString = `${monthName} ${year}`;
-      monthYearArray.push(monthYearString);
+      const result = allMonths.map(month => {
+        const monthYearString = format(month, 'MMMM yyyy');
+        const dt = startOfMonth(month);
+        return { dt, monthYearString };
+      });
+        
+      this.deliveryMonths = result.reverse();
+    },
+    handleDropdownChange() {
+      if(this.selectedMpan === null || this.selectedMonth === null)
+      {
+        return;
       }
-  
-      this.deliveryMonths = monthYearArray;
-      console.log(this.deliveryMonths);
-    }
+
+      this.selectedFromDate = format(this.selectedMonth,'yyyy-MM-dd')
+      this.selectedToDate = format(endOfMonth(this.selectedMonth),'yyyy-MM-dd');
+      this.fetchDuosData();
+    },    
   },
   mounted(){
+    this.fetchMpanData();
     this.generateDeliveryMonthArray();
   }
 };
@@ -61,81 +107,85 @@ export default {
   <Layout>
     <PageHeader :title="title" :items="items" />
     <b-row>
-      <b-col xxl="5">
+
         <b-row>
           <b-col cols="12">
             <b-card no-body>
               <b-card-body class="p-0">
  
-                <div class="input-group">
-                        <label class="input-group-text" style="width:150px" for="inputGroupSelect01">Delivery Month</label>
-                        <select class="form-select" id="inputGroupSelect01" v-model="selectedMonth">
-                          <option v-for="monthYear in deliveryMonths" :key="monthYear" :value="monthYear">{{ monthYear }}</option>
+                      <div class="input-group">
+                        <label class="input-group-text" style="width:150px" for="igDateType" >Month Type</label>
+                        <select class="form-select" id="igDateType" v-model="selectedMonthType" @change="handleDropdownChange">
+                          <option value="1">Delivery</option>
+                          <option value="2">Invoice</option>
+                        </select>
+                        <label class="input-group-text" style="width:150px" for="igDateType">Month</label>
+                        <select class="form-select" id="igMonth" v-model="selectedMonth" @change="handleDropdownChange">
+                          <option v-for="monthYear in deliveryMonths" :key="monthYear.dt" :value="monthYear.dt">{{ monthYear.monthYearString }}</option>
                         </select>
                       </div>
 
                       <div class="input-group">
-                        <label class="input-group-text" style="width:150px" for="inputGroupSelect01">MPAN</label>
-                        <select class="form-select" id="inputGroupSelect01">
-                            <option value="1">EELC</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
+                        <label class="input-group-text" style="width:150px" for="igMpan">MPAN</label>
+                        <select class="form-select" id="igMpan" v-model="selectedMpan" @change="handleDropdownChange">
+                          <option v-for="mpan in mpanList" :key="mpan.mpanCore" :value="mpan.mpanCore">{{ mpan.mpanCore }}</option>
                         </select>
                       </div>
-
-                <ul class="nav nav-pills nav-custom nav-custom-light mb-3" role="tablist">
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#nav-light-home" target="_self" data-bs-toggle="tab" role="tab"> Active </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#nav-light-profile" target="_self" data-bs-toggle="tab" role="tab"> Reactive </a>
-                  </li>
-                </ul>
-
-
-                <b-row class="align-items-end">
-                  <b-col sm="12">
-                    <div class="p-3">
-
-
-
-
-                      <div class="mt-3 table-hscroll">
-                        <table class="table table-nowrap table-hover">
-                          <thead>
-                              <tr>
-                                <th scope="col">Date</th>
-                                  <!-- Generate columns up to 50 -->
-                                  <template v-for="column in 50" :key="column">
-                                    <th scope="col" >{{ column }}</th>
-                                  </template>
-                              </tr>
-                          </thead>
-                          <tbody>
-                              <!-- Generate rows for each day of the month -->
-                              <template v-for="day in 30" :key="day">
-                                <tr>
-                                  <th scope="row">
-                                    <a href="#" class="fw-semibold">{{ day }}</a>
-                                  </th>
-                                  <template v-for="column in 50" :key="column">
-                                    <td>1.02</td>
-                                  </template>
-                                </tr>
-                              </template>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </b-col>
-                  <b-col sm="4">
-                  </b-col>
-                </b-row>
+                            <b-row class="align-items-end">
+                              <b-col sm="12">
+                                <div class="p-3">
+                                  <div v-if="isDataAvailable">
+                                    <div class="mt-3 table-hscroll">
+                                      <table class="table table-nowrap table-hover">
+                                          <thead>
+                                            <tr>
+                                              <th>Invoice Date</th>
+                                              <th>Period Start</th>
+                                              <th>Period End</th>
+                                              <th>HH/NHH</th>
+                                              <th>Invoice Type</th>
+                                              <th>Tariff Code</th>
+                                              <th>Days Billed</th>
+                                              <th>LLF</th>
+                                              <th>Charge Description</th>
+                                              <th>UoM</th>
+                                              <th>Units Billed</th>
+                                              <th>Price Per Unit</th>
+                                              <th>Total Charge</th>
+                                            </tr>
+                                          </thead>
+                                          <tbody>
+                                            <tr v-for="item in duosData" :key="item.invoiceDate">
+                                              <td>{{ new Date(item.invoiceDate).toLocaleDateString("en-GB") }}</td>
+                                              <td>{{ new Date(item.periodStart).toLocaleDateString("en-GB") }}</td>
+                                              <td>{{ new Date(item.periodEnd).toLocaleDateString("en-GB") }}</td>
+                                              <td>{{ item.hhnhh }}</td>
+                                              <td>{{ item.invoiceType }}</td>
+                                              <td>{{ item.tariffCode }}</td>
+                                              <td>{{ item.daysBilled }}</td>
+                                              <td>{{ item.llf ? item.llf : 'N/A' }}</td>
+                                              <td>{{ item.chargeDescription }}</td>
+                                              <td>{{ item.uoM }}</td>
+                                              <td>{{ item.unitsBilled }}</td>
+                                              <td>{{ item.pricePerUnit.toFixed(2) }}</td>
+                                              <td>{{ item.totalCharge.toFixed(2) }}</td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+                                    </div>
+                                  </div>
+                                  <div v-else class="centered-children-200">
+                                    <p>No Data Available</p>
+                                  </div>   
+                                </div>                               
+                              </b-col>
+                              <b-col sm="4">
+                              </b-col>
+                            </b-row>
               </b-card-body>
             </b-card>
           </b-col>
         </b-row>
-      </b-col>
     </b-row>
   </Layout>
 </template>
